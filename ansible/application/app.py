ec480fc2518578dfad4d03f0f4683695d2d9dc01
“jorisdejosselintrue” <joris.josselin@true.nl>
2024-09-04 16:52:24 +0200

    Add working ansible playbook

--- ansible/application/app.py
+++ ansible/application/app.py
@@ -1,21 +1,15 @@
-from flask import Flask, render_template
 import os
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Get the number of drinks from environment variable or default to 5
-    number_of_drinks = int(os.getenv('NUMBER_OF_DRINKS', 5))
+    drinks = json.loads(os.environ['DRINKS'])
 
-    # Sample list of drinks
-    all_drinks = [
-        "Coca Cola", "Pepsi", "Sprite", "Fanta", "Mountain Dew",
-        "Dr Pepper", "7 Up", "Ginger Ale", "Root Beer", "Lemonade"
-    ]
-
-    # Limit the number of drinks to be displayed
-    drinks = all_drinks[:number_of_drinks]
+    if drinks['barman']['amount_of_drinks_drunken'] > 15:
+        raise RuntimeError("Barman has had too many drinks!")
 
     return render_template('index.html', drinks=drinks)
 
