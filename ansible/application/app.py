813f018ed9bf4148e1a51bdbf61c7477707631af
Jona Koudijs <jona@koudijs.me>
2024-09-19 16:01:38 +0200

    Add custom 500 error page with drunk gif

--- ansible/application/app.py
+++ ansible/application/app.py
@@ -16,5 +16,10 @@ def index():
 
     return render_template('index.html', drinks=drinks)
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
